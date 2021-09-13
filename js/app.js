15eebd7d8e4792928e49e9907f7fdb3d1e2020bb
imtiazpy <ahmed.imti1994@gmail.com>
2021-09-13 22:47:54 +0600

    refactored the codebase and added details button feature

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const allProducts = document.getElementById('all-products');
 const searchField = document.getElementById('search-field');
 const searchBtn = document.getElementById('search-btn');
+const totalProducts = document.getElementById('total-Products');
 let count = 0;
 
 // function for loading all the products from the API 
@@ -15,7 +16,7 @@ const loadProducts = () => {
 // show all product in UI 
 const showProducts = (products) => {
 	for (const product of products) {
-		const { title, price, category, image, rating: { rate, count } } = product;
+		const { id, title, price, category, image, rating: { rate, count } } = product;
 		const div = document.createElement("div");
 		div.classList.add("col", "single-product");
 		div.innerHTML = `
@@ -27,12 +28,11 @@ const showProducts = (products) => {
 					<h5 class="card-title">${title}</h5>
 					<p class="card-text">Category: ${category}</p>	
       			</div>
-      		
 				<div class="card-footer">
 					<h4 class="text-center">Price: $ ${price}</h4>
 					<div class="d-flex justify-content-evenly">
 						<button onclick="addToCart(${price})" id="addToCart-btn" class="buy-now btn btn-outline-success">add to cart</button>
-						<button id="details-btn" class="btn btn-outline-info">Details</button>
+						<button id="details-btn" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetail(${id})">Details</button>
 					</div>
 					<div class="d-flex justify-content-evenly mt-2">
 						<button class="btn btn-info">${rate} <i class="fas fa-star"></i>
@@ -46,23 +46,32 @@ const showProducts = (products) => {
 	}
 };
 
-// function for adding products to cart 
-const addToCart = (price) => {
-	count++;
-	toggleError(false)
-	updatePrice('price', price);
-	updateTaxAndCharge();
-	updateTotal()
-	document.getElementById("total-Products").innerText = count;
-};
+// show single product info in the modal on button click
+const showDetail = id => {
+	const detailCard = document.getElementById('detail-card');
+	detailCard.textContent = '';
+	const url = `https://fakestoreapi.com/products/${id}`;
+	fetch(url)
+		.then(res => res.json())
+		.then(product => {
+			const { title, description, image } = product;
+			detailCard.innerHTML = `
+				<div class="col">
+					<div class="card border border-info">
+						<div class="card-body text-center">
+							<div class="border border-info rounded-3 p-2 mb-2">
+								<img class="card-img-top product-image" src="${image}"/>
+							</div>
+							<h5 class="card-title">${title}</h5>
+							<p class="card-text">${description}</p>	
+	  					</div>
+					</div>
+				</div>
+			`
+		})
+}
 
-// main price update function
-const updatePrice = (id, value) => {
-	const previousProductPrice = getValue(id);
-	const newProductPrice = parseFloat(value);
-	const total = previousProductPrice + newProductPrice;
-	document.getElementById(id).innerText = total.toFixed(2);
-};
+
 
 // get value from any amount field by ID 
 const getValue = (id) => {
@@ -75,6 +84,26 @@ const getValue = (id) => {
 const setValue = (id, val) => {
 	const value = parseFloat(val)
 	document.getElementById(id).innerText = value.toFixed(2);
+};
+
+
+// function for adding products to cart 
+const addToCart = (price) => {
+	count++;
+	// disabling err message if it was enabled on buyNow button click
+	toggleError(false)
+	updatePrice('price', price);
+	updateTaxAndCharge();
+	updateTotal()
+	totalProducts.innerText = count;
+};
+
+// main price update function
+const updatePrice = (id, value) => {
+	const previousProductPrice = getValue(id);
+	const newProductPrice = parseFloat(value);
+	const total = previousProductPrice + newProductPrice;
+	setValue('price', total)
 };
 
 // update delivery charge and total Tax
@@ -100,40 +129,48 @@ const updateTotal = () => {
 	const deliveryCharge = getValue('delivery-charge');
 	const tax = getValue('total-tax');
 	const grandTotal = price + deliveryCharge + tax;
-	setValue('total', grandTotal.toFixed(2));
-	// document.getElementById("total").innerText = grandTotal.toFixed(2);
+	setValue('total', grandTotal);
 };
 
+// clear all the value on buyNow btn click
 const buyNowHandler = () => {
 	const addedProducts = count
 	const cartAmountFields = ['price', 'delivery-charge', 'total-tax', 'total'];
 	cartAmountFields.forEach(field => {
 		if (addedProducts === 0) {
+			// if any products were not added the error message will be shown 
 			toggleError("No products were added to cart! Please add some", true)
 		} else {
-			toggleError(false)
 			setValue(field, 0)
 		}
 	});
 	count = 0
-	document.getElementById("total-Products").innerText = count;
+	// setting the initial delivery charge and products count 
+	document.getElementById('delivery-charge').innerText = 20;
+	totalProducts.innerText = count;
 }
 
+// function to toggle error message on and off
 const toggleError = (msg = '', toShow) => {
-	const errorMsg = document.getElementById('error-message');
+	const alertMsg = document.getElementById('alert-message');
 	if (toShow) {
-		errorMsg.innerText = msg;
-		errorMsg.classList.remove('d-none');
+		alertMsg.innerText = msg;
+		alertMsg.classList.remove('d-none');
 	} else {
-		errorMsg.classList.add('d-none')
+		alertMsg.classList.add('d-none')
 	}
 }
 
 
+
+
 // search functionality
 const searchProduct = () => {
+	// taking all the cards in an array with classname 
 	const productList = document.getElementsByClassName('single-product')
 	const searchKey = searchField.value.toLowerCase();
+
+	// if the searchKey is available in any of the cards then those cards will be shown
 	for (const product of productList) {
 		product.innerText.toLowerCase().includes(searchKey) ?
 			product.style.display = 'block' : product.style.display = 'none';
@@ -147,24 +184,3 @@ searchBtn.addEventListener('click', (e) => {
 
 // showing products initially
 loadProducts();
-
-
-
-/*
-tried to show stars according to the data value, but could quite get it done.
-it's taking way too much time to show the stars on each card in each iteration.
-Need to work on it later
-*/
-
-// const ratingsHandler = (rate) => {
-// 	let star;
-// 	if (rate >= 4) {
-// 		star = '<i class="fas fa-star"></i>'.repeat(4)
-// 	} else if (rate >= 3) {
-// 		star = '<i class="fas fa-star"></i>'.repeat(3)
-// 	} else {
-// 		star = '<i class="fas fa-star"></i>'.repeat(2)
-// 	}
-// 	return star
-// }
-
